87d37bb07bedb9283e11f4ce6a2df1a7a69c98db
MikeYoulend <mikeyoulend@gmail.com>
2024-02-10 13:13:12 +0100

    Const

---
+++ app.js
@@ -0,0 +1,3 @@
+const computerChoice = document.getElementById("computer-choice"); //Prendo dal documento lo span con questo id
+const userChoice = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
